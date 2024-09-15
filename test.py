9f40a99b2dc3201f1303805748ae252c49d1fc33
CoderBak <hx-sun@outlook.com>
2024-09-15 16:29:23 +0800

    Update

--- test.py
+++ test.py
@@ -1,6 +1,19 @@
 import matheval
 
-engine = matheval.MathEval("/root/autodl-tmp/qwen2-1.5b", seed=42)
+# You can set load-model arguments: tensor_parallel_size, dtype, quantization, seed, etc.
+engine = matheval.MathEval("/media/public/models/huggingface/Qwen/Qwen2-72B-Instruct", seed=42)
+
+# You should set sampling args before inferencing.
+# best_of, presence_penalty, frequency_penalty, repetition_penalty, temperature
+# top_p, top_k, min_p, seed, use_beam_search, length_penalty, early_stopping
+# stop, max_tokens, min_tokens, etc.
 engine.set_sampling_args(temperature=0.1)
-print(engine.generate(["You are a helpful assistant. 今天天气如何"]))
+
+# Use generate to generate results.
+print(engine.generate([
+    "You are a helpful assistant. 今天天气如何",
+    "讲个笑话吧"
+]))
+
+# Use evaluation to evaluate the model on the given dataset.
 engine.evaluation("gsm8k")
