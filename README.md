2e214b8dcc62567f9d6bea8d867d8ac26aeec9b2
CoderBak <hx-sun@outlook.com>
2024-09-18 15:19:05 +0800

    Update

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 ### Install
 
 ```bash
-conda create -n llm-math python=3.10
+conda create -n llm_math python=3.10
 pip install -r requirements.txt
 ```
 
@@ -60,17 +60,17 @@ pip install -r requirements.txt
 ### Example
 
 ```python
-import llm-math
+import llm_math
 
-llm-math.set_seed(42)
-print(llm-math.basic_check('(0.6,3.6667]', '(\\frac{3}{5},\\frac{8}{3} + 1]'))
-print(llm-math.check(
+llm_math.set_seed(42)
+print(llm_math.basic_check('(0.6,3.6667]', '(\\frac{3}{5},\\frac{8}{3} + 1]'))
+print(llm_math.check(
     prompt_type="cot",
     data_name="gsm8k",
     target={"question":"Kalinda is working on a 360 piece puzzle with her mom. Kalinda can normally add 4 pieces per minute. Her mom can typically place half as many pieces per minute as Kalinda.  How many hours will it take them to complete this puzzle?","answer":"Her mom places 2 pieces per minute because 4 \/ 2 = <<4\/2=2>>2\nOn average they get in 6 pieces per minute because 4 + 2 = <<4+2=6>>6\nIt will take 60 minutes to complete the puzzle because 360 \/ 6 = <<360\/6=60>>60\nIt will take one hour because 60 \/ 60 = <<60\/60=1>>1\n#### 1","idx":228},
     pred="Kalinda can add 4 pieces per minute. Her mom can add half as many pieces per minute as Kalinda. So her mom can add 2 pieces per minute. 360 pieces divided by 4 is 90. 90 divided by 2 is 45. 45 minutes is 1 hour. The answer is 1 hour."
 ))
-engine = llm-math.Model("/media/public/models/huggingface/meta-llama/Llama-2-7b-hf", enforce_eager=False)
+engine = llm_math.Model("/media/public/models/huggingface/meta-llama/Llama-2-7b-hf", enforce_eager=False)
 engine.set_sampling_args(temperature=0, max_tokens=2048)
 engine.test(
     datasets=["gsm8k", "math", "svamp", "asdiv", "mawps", "tabmwp", "mathqa", "mmlu_stem", "sat_math"],
--- test.py
+++ test.py
@@ -1,17 +1,17 @@
-import matheval
+import llm_math
 
-matheval.set_seed(42)
+llm_math.set_seed(42)
 
-print(matheval.basic_check('(0.6,3.6667]', '(\\frac{3}{5},\\frac{8}{3} + 1]'))
+print(llm_math.basic_check('(0.6,3.6667]', '(\\frac{3}{5},\\frac{8}{3} + 1]'))
 
-print(matheval.check(
+print(llm_math.check(
     prompt_type="cot",
     data_name="gsm8k",
     target={"question":"Kalinda is working on a 360 piece puzzle with her mom. Kalinda can normally add 4 pieces per minute. Her mom can typically place half as many pieces per minute as Kalinda.  How many hours will it take them to complete this puzzle?","answer":"Her mom places 2 pieces per minute because 4 \/ 2 = <<4\/2=2>>2\nOn average they get in 6 pieces per minute because 4 + 2 = <<4+2=6>>6\nIt will take 60 minutes to complete the puzzle because 360 \/ 6 = <<360\/6=60>>60\nIt will take one hour because 60 \/ 60 = <<60\/60=1>>1\n#### 1","idx":228},
     pred="Kalinda can add 4 pieces per minute. Her mom can add half as many pieces per minute as Kalinda. So her mom can add 2 pieces per minute. 360 pieces divided by 4 is 90. 90 divided by 2 is 45. 45 minutes is 1 hour. The answer is 1 hour."
 ))
 
-print(matheval.check(
+print(llm_math.check(
     prompt_type="cot",
     data_name="mmlu_stem",
     target={"question":"In Sweden, the red fox (Vulpes vulpes) severely limits populations of its prey, including hares. However, red fox populations are sometimes attacked by a fatal parasite, the mange mite. As mite population sizes increase at a given site, how are hare and fox populations most likely to respond at the same site? (Assume that hares have no major predators at this site other than foxes.)","type":"college_biology","choices":["Both fox and hare populations will decrease.","Both fox and hare populations will increase.","Fox populations will decrease and hare populations will increase.","Fox populations will increase and hare populations will decrease."],"answer":2},
@@ -19,7 +19,7 @@ print(matheval.check(
 ))
 
 # You can set load-model arguments: tensor_parallel_size, dtype, quantization, etc.
-engine = matheval.MathEval("/media/public/models/huggingface/meta-llama/Llama-2-7b-hf", enforce_eager=False)
+engine = llm_math.Model("/media/public/models/huggingface/meta-llama/Llama-2-7b-hf", enforce_eager=False)
 
 # You should set sampling args before inferencing.
 # best_of, presence_penalty, frequency_penalty, repetition_penalty, temperature
